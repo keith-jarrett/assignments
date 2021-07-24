6263e59ac8ce5abb71e71c9a4001d04a85eafff8
Keith Jarrett <keithedwardjarrett@gmail.com>
2021-07-23 17:11:57 -0700

    I have rewritten FindChar so that it reads from a textfile.

--- DayThreeAssignment/src/FindChar.java
+++ DayThreeAssignment/src/FindChar.java
@@ -1,16 +1,42 @@
-public class FindChar    
-{    
-    public static void main(String[] args) {    
-        String string = "The best of both worlds";    
-        int count = 0;    
-            
-        //Counts each character except space    
-        for(int i = 0; i < string.length(); i++) {    
-            if(string.charAt(i) != ' ')    
-                count++;    
-        }    
-            
-        //Displays the total number of characters present in the given string    
-        System.out.println("Total number of characters in a string: " + count);    
-    }    
-}     
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FindChar {
+
+ public static void main(String[] args) {
+  BufferedReader objReader = null;
+  try {
+   String strCurrentLine;
+   long total = 0;
+   // read the object in and store in an object
+   objReader = new BufferedReader(new FileReader("textfile.txt"));
+
+  // This reads through the file line by line.
+   while ((strCurrentLine = objReader.readLine()) != null) {
+     // this counts the number of occurences of the given character.
+     // .chars() returns in integer stream. Each integer is the unicode Integer
+     // value for each character.
+     // .filter() returns a stream that matches the condition in the parenthesis
+     // .count() counts the number of elements in a stream. It is type long,
+     // that's why the return value of this is long.
+    long count = strCurrentLine.chars().filter(ch -> ch == 'e').count();
+
+    total += count;
+   }
+   System.out.println("The character 'e' appears in the text " + total + " times.");
+  } catch (IOException e) {
+
+   e.printStackTrace();
+
+  } finally {
+    // close the object.
+   try {
+    if (objReader != null)
+     objReader.close();
+   } catch (IOException ex) {
+    ex.printStackTrace();
+   }
+  }
+ }
+}
